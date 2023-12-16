718cf2608ca128bfce69cd1c6b1a9778a36882a8
email <tarunmehto71@gmail.com>
2023-12-16 10:37:42 +0530

    done

--- index.js
+++ index.js
@@ -43,6 +43,45 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/new", (req, res) => {
+  const date = new Date();
+  const formattedDateMumbai = format(date, "yyyy-MM-dd HH:mm:ssXXX", {
+    timeZone: "Asia/Kolkata",
+  });
+
+  const formattedDateTokyo = format(date, "yyyy-MM-dd HH:mm:ssXXX", {
+    timeZone: "Asia/Tokyo",
+  });
+
+  // Display in London time zone
+  const formattedDateLondon = format(date, "yyyy-MM-dd HH:mm:ssXXX", {
+    timeZone: "Europe/London",
+  });
+  console.log("Formatted Date in London Time Zone:", formattedDateLondon);
+
+  // Display in Sydney time zone
+  const formattedDateSydney = format(date, "yyyy-MM-dd HH:mm:ssXXX", {
+    timeZone: "Australia/Sydney",
+  });
+  console.log("Formatted Date in Sydney Time Zone:", formattedDateSydney);
+
+  // Display in Mumbai time zone
+
+  console.log("date", date);
+  console.log("Asia/Tokyo", formattedDateTokyo);
+  console.log("Europe/London", formattedDateLondon);
+  console.log("Australia/Sydney", formattedDateSydney);
+  console.log("Asia/Kolkata", formattedDateMumbai);
+
+  res.send({
+    date,
+    formattedDateLondon,
+    formattedDateMumbai,
+    formattedDateSydney,
+    formattedDateTokyo,
+  });
+});
+
 app.listen(`${process.env.port}` || 5500, () => {
   console.log(`running server on port ${process.env.port}`);
 });
